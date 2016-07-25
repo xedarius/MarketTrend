2cc2861eb4da22108262d93794c684d217d0a9b5
Rich Carless <xedarius@gmail.com>
2016-07-25 21:47:37 +0100

    added main file for directory structure

---
+++ rsclabs-markettrend/src/main/java/com/rsclabs/main.java
@@ -0,0 +1,12 @@
+package com.rsclabs;
+
+/**
+ * Created by rich on 25/07/2016.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("hello");
+    }
+}
